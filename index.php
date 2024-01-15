bb77eadc86f6afc41c0e2e3428bf1ca5fa71c8c3
Raphaël <115149179+rboucheron@users.noreply.github.com>
2024-01-15 21:41:52 +0100

    update

---
+++ index.php
@@ -0,0 +1,258 @@
+
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Chiots Finnois de Laponie - la portée de Sally</title>
+  <meta name="description" content="Découvrez nos chiots Finnois de Laponie. Consultez des informations sur notre portée récente et notre passion pour les chiens. Contactez-nous pour adopter un adorable compagnon.">
+  <meta name="keywords" content="chiots Finnois de Laponie, chiots, élevage familial, chien, chien reproducteur, adoption chiots, compagnon, 4 pates, race nordique, Sally, portée récente, adoption chiots 2024, éleveur responsable">
+  <meta property="og:title" content="Des Chiots Finnois de Laponie">
+  <meta property="og:description" content="Découvrez notre élevage familial passionné.">
+  <meta property="og:image" content="./assets/sallychiot3.png">
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="./src/css/mind.css">
+</head>
+
+<body class="bg-mauve">
+  <header class="border border-bottom-1 border-blue-sky custom-sticky bg-mauve z-3 top-0">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <img src="./assets/thedogslogo.svg" alt="Sally allaitant ses chiots">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link custom-link color-blue" href="#Accueil" data-target="#navbarNav">Accueil</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link custom-link color-blue" href="#Information" data-target="#navbarNav">Information</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link custom-link color-blue" href="#Portee" data-target="#navbarNav">La Portée</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link custom-link color-blue" href="#Apropos" data-target="#navbarNav">À Propos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link custom-link color-blue" href="#Galerie" data-target="#navbarNav">Galerie</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link custom-link color-blue" href="#Contact" data-target="#navbarNav">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+  </header>
+  <?php if (isset($message)) { ?>
+
+    <div class="modal" tabindex="-1">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Votre Mail</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <p><?= $message ?></p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  <?php } ?>
+  <section class="container mt-5x2 mb-5x2 mb-5x3 mb-2" id="Accueil">
+    <div class="row">
+      <div class="col-md-5">
+        <h1 class="mt-sm-5 text-center color-blue-sky display-4 text-Alata">La portée de Sally</h1>
+        <p class="color-blue fs-4 text-poppins">Le 22 décembre 2023, notre chienne Sally, de race Finnois de Laponie, a
+          accueilli une portée de 4 chiots, comprenant 3 femelles et 1 mâle. Ces nouveaux membres sont élevés avec amour
+          et attention, prêts à apporter joie et bonheur dans leur future maison. Nous sommes impatients de partager
+          cette merveilleuse expérience avec vous.</p>
+      </div>
+      <div class="col-1"></div>
+      <div class="col-md-6">
+        <img src="./assets/sallychiot3.png" class="d-block w-100 rounded-2" alt="Sally allaitant ses chiots">
+      </div>
+    </div>
+  </section>
+  <section class="container mt-5 mb-5x3 " id="Information">
+    <h2 class="w-100 text-center color-blue-sky display-4 text-Alata">Information</h2>
+    <div class="row mt-5 mx-auto">
+      <div class="col-sm-1"></div>
+      <div class="col-sm-5 mb-5x2">
+        <p class="w-100 text-start color-blue fs-5 mt-2  text-Alata">
+          Roi Iorek est un mâle reproducteur Finois de Laponie âgé de 3 ans inscrit et confirmé au LOF. Il vit
+          au domaine de Tangusko en Vendée. Roi Iorek et Sally se sont rencontrés le 24 octobre 2023
+        </p>
+        <img src="./assets/043.jpg" alt="Le roi Iorek, mâle reproducteur de la race Finnois de Laponie, est le père de cette portée" class="cover w-100 h-80 dimension">
+      </div>
+      <div class="col-sm-5">
+        <p class="w-100 text-start color-blue fs-5  mt-2 text-Alata">
+          Sally est une femelle âgée de 2 ans Finoise de Laponie inscrite et confirmée au LOF. Elle vit en famille
+          au nord de la Seine et Marne.
+        </p>
+        <img src="./assets/Sally.png" alt="Sally, femelle reproductrice de la race Finnois de Laponie, est la mère de cette portée" class="cover dimension">
+      </div>
+    </div>
+
+  </section>
+  <div class="container-fluid mt-5x2" id="Portee">
+    <h2 class="w-100 text-center color-blue-sky display-4 text-Alata mb-5">Notre portée
+    </h2>
+    <div class="row">
+      <div class="col-2">
+      </div>
+      <div class="d-flex justify-content-center col-12 col-sm-4 sm-m-2">
+        <div class="card " style="width: 100%;">
+          <img src="./assets/coliergris.jpg" class="card-img-top" alt="Petite chienne au collier blanc sur une nappe rouge." style="height: 265px; object-fit: cover;">
+          <div class="card-body">
+            <h3 class="card-title text-Alata color-accent">Collier Gris</h3>
+            <p class="card-text">Une femelle de couleur Sable, agée de 10 jours sur la photo</p>
+
+          </div>
+        </div>
+      </div>
+      <div class="d-flex justify-content-center  col-12 col-sm-4 sm-m-2">
+        <div class="card " style="width: 100%;">
+          <img src="./assets/coliervert.jpg" class="card-img-top" alt="Petite chienne au collier vert sur une nappe rouge." style="height: 265px; object-fit: cover;">
+          <div class="card-body">
+            <h3 class="card-title text-Alata color-accent">Collier Vert</h3>
+            <p class="card-text">Une femelle de couleur Sable, agée de 10 jours sur la photo</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-2">
+      </div>
+    </div>
+    <div class="row mt-3">
+      <div class="col-2">
+      </div>
+      <div class="d-flex justify-content-center col-12 col-sm-4 sm-m-2">
+        <div class="card " style="width: 100%;">
+          <img src="./assets/colierrose.jpg" class="card-img-top" alt="Petite chienne au collier rose sur une nappe rouge" style="height: 265px; object-fit: cover;">
+          <div class="card-body">
+            <h3 class="card-title text-Alata color-accent">Collier Rose</h3>
+            <p class="card-text">Une femelle de couleur Sable, agée de 10 jours sur la photo</p>
+          </div>
+          <div class="reserve">En cours d'adoption</div>
+        </div>
+      </div>
+      <div class="d-flex justify-content-center col-12 col-sm-4 sm-m-2">
+        <div class="card " style="width: 100%;">
+          <img src="./assets/colierjaune.jpg" class="card-img-top" alt="Petit chient au collier jaune qui se repose sur une nappe rouge." style="height: 265px; object-fit: cover;">
+          <div class="card-body">
+            <h3 class="card-title text-Alata color-accent">Collier Jaune</h3>
+            <p class="card-text">Un Mâle de couleur noir et sable, agé de 10 jours sur la photo </p>
+          </div>
+          <div class="reserve">En cours d'adoption</div>
+        </div>
+      </div>
+      <div class="col-2">
+      </div>
+    </div>
+  </div>
+  <section class="container mt-5x3" id="Apropos">
+    <h2 class="w-100 text-center color-blue-sky display-4 text-Alata">Qui Sommes nous ?</h2>
+    <div class="mt-5 row">
+      <div class="col-sm-5">
+        <p class="color-blue fs-4 text-poppins "> Nos chiots sont élevés au sein de notre famille, bénéficient
+          d'attention et de
+          carresses. Nos chiots seront prêts à rejoindre leur nouveau foyer dès mi-mars 2024, à partir de cette date,
+          vous pourrez accueillir chez vous l'un de ces adorables compagnons.</p>
+      </div>
+
+      <div class="col-sm-7">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d20981.981020052983!2d2.7916749790780435!3d48.90114666560703!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e61d8e1ca49d35%3A0x40b82c3688c5b80!2sEsbly!5e0!3m2!1sfr!2sfr!4v1705326078985!5m2!1sfr!2sfr" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+    </div>
+  </section>
+  <section class="container mt-5x3" id="Galerie">
+    <h2 class="w-100 text-center color-blue-sky display-4 mb-5 text-Alata">Galerie</h2>
+    <div class="row">
+      <div class="col-sm-4 mt-2">
+        <img src="./assets/7f541879-871a-4d6e-885c-dffb29ea2d68.JPG" class="rounded cover" alt="Tous les chiots de la portée qui jouent dans leur enclos" height="80%" width="100%">
+      </div>
+      <div class="col-sm-3 mt-2">
+        <img src="./assets/IMG_6961.jpeg" class="rounded cover" alt="les chiots Collier Vert, Collier Jaune et Collier Rose, en train de dormir confortablement sur leurs tapis." height="80%" width="100%">
+      </div>
+      <div class="col-sm-5 mt-2">
+        <img src="./assets/b2029860-34d6-4e4a-8808-dee816ff4a45.JPG" class="rounded cover" alt="Les chiots Collier Vert, Collier Jaune et Collier Rose, qui essaye de s'enfuir" height="80%" width="100%">
+      </div>
+    </div>
+    <div class="row ">
+      <div class="col-sm-3 mt-2">
+        <img src="./assets/6727b33b-8640-4bd8-8f7c-af5fb5ade4f6.JPG" class="rounded cover" alt="Le chiot au collier blanc qui vient de se réveiller" height="80%" width="100%">
+      </div>
+      <div class="col-sm-6 mt-2">
+        <img src="./assets/sallychiot4.png" class="rounded cover" alt="Les chiots pendant leur période d'allaitement" height="80%" width="100%">
+      </div>
+      <div class="col-sm-3 mt-2">
+        <img src="./assets/07cee3c5-2c3b-4fc0-89c7-31b061c7482a.JPG" class="rounded cover" alt="Chiot au collier rose et chiot au collier noir dormant l'un sur l'autre." height="80%" width="100%">
+      </div>
+    </div>
+  </section>
+  <section id="Contact">
+    <div class="container mt-5x3">
+      <div class="row">
+        <h2 class="mt-sm-5 text-center color-blue-sky w-100 display-4 text-Alata">Nous Contacter</h2>
+      </div>
+      <div class="row">
+        <div class="col-sm-3"></div>
+        <div class="col-sm-6">
+
+          <form action="" method="post">
+            <div class="mt-5 mb-3">
+              <label for="exampleInputEmail1" class="form-label">Nom</label>
+              <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="nom" required="required">
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">Prénom</label>
+              <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="prenom" required="required">
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">Email</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" required="required">
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">Votre message</label>
+              <textarea type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="content" required="required"></textarea>
+            </div>
+            <button type="submit" class="btn bg-blue color-mauve">Envoyer</button>
+          </form>
+
+          <div class="col-3"></div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <footer class="bg-light py-4 mt-5x3">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+
+          <img src="./assets/thedogslogo.svg" alt="Votre Logo" width="40%" class="img-fluid">
+        </div>
+        <div class="col-md-4">
+
+          <h4 class="text-center">Les Petits de Sally</h4>
+        </div>
+        <div class="col-md-4 text-center">
+
+          <p>Tous droits réservés © 2024</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+</body>
+
+</html>
